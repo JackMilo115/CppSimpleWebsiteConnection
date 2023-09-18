b61029406c1be82118f1001f6d0b5e99e45fbf79
Jack Milo <mil21001248@btc.ac.uk>
2023-09-18 15:27:28 +0100

    Made the response reading asynchronous.

--- SimpleWebsite/SimpleWebsite.cpp
+++ SimpleWebsite/SimpleWebsite.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #ifdef _WIN32
 #define _WIN32_WINNIT 0x0A00
@@ -8,16 +9,50 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+// Create reusable buffer to store returned data
+std::vector<char> vBuffer(20 * 1024);
+
+// Asynchronously read returned data
+void GrabSomeData(asio::ip::tcp::socket& socket)
+{
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](std::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				std::cout << "\n\nRead " << length << " bytes\n\n";
+
+				for (int i = 0; i < length; i++)
+				{
+					std::cout << vBuffer[i];
+				}
+
+				// Continues grabbing data until there is none left.
+				GrabSomeData(socket);
+			}
+		}
+	);
+}
 int main()
 {
+	// Create error code storage
 	asio::error_code ec;
+
 	// Create a context
 	asio::io_context context;
+
+	// Give some fake tasks to asio so the context doesn't finish
+	asio::io_context::work idleWork(context);
+
+	// Run the context in its own thread
+	std::thread thrContext = std::thread([&]() { context.run(); });
+
 	// Get the address of somewhere we with to connect to - example.com
-	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
-	//asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", ec), 80);
+
 	// Create a socket
 	asio::ip::tcp::socket socket(context);
+
 	// Tell socket to try and connect
 	socket.connect(endpoint, ec);
 
@@ -32,6 +67,8 @@ int main()
 
 	if (socket.is_open())
 	{
+		GrabSomeData(socket);
+
 		// Compose request
 		std::string sRequest =
 			"GET /index.html HTTP/1.1\r\n"
@@ -41,23 +78,14 @@ int main()
 		// Send request
 		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
 
-		// Wait for response
-		socket.wait(socket.wait_read);
+		// Manually wait
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(20000ms);
 
-		// Check bytes returned
-		size_t bytes = socket.available();
-		std::cout << "Bytes Available " << bytes << std::endl;
-
-		// Print bytes returned
-		if (bytes > 0)
+		context.stop();
+		if (thrContext.joinable())
 		{
-			std::vector<char> vBuffer(bytes);
-			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-			for (auto c : vBuffer)
-			{
-				std::cout << c;
-			}
+			thrContext.join();
 		}
 	}
 
