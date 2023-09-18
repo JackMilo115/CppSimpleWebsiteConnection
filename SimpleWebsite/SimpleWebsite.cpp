5afe437ef37502824ec6cc520583becac47aa08e
Jack Milo <mil21001248@btc.ac.uk>
2023-09-18 14:44:45 +0100

    Added test value.

--- SimpleWebsite/SimpleWebsite.cpp
+++ SimpleWebsite/SimpleWebsite.cpp
@@ -14,13 +14,14 @@ int main()
 	// Create a context
 	asio::io_context context;
 	// Get the address of somewhere we with to connect to - example.com
-	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+	//asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
 	// Create a socket
 	asio::ip::tcp::socket socket(context);
 	// Tell socket to try and connect
 	socket.connect(endpoint, ec);
 
-	if (!ec) // Check if error
+	if (!ec) // Check if error code exists.
 	{
 		std::cout << "Connected!" << std::endl;
 	}
